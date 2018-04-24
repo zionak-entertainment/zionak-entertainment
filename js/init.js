784d810f107c26d8903ec69887108e125ff94d24
Carlos Eduardo Perez Villanueva <carlospe097@outlook.com>
2018-04-23 19:53:16 -0600

    Re-positioned the hero image  label, changed the hero image, added a carousel below "about us", improved the nav bar's responsiveness.

--- js/init.js
+++ js/init.js
@@ -6,3 +6,14 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    duration: 200
+  });
+
+  autoplay()   
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
